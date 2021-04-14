b59fa161e7c255ff52ade21ca8400bd623220b90
Shawn Littrel <76717896+shawnlittrel@users.noreply.github.com>
2021-04-13 21:31:45 -0700

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection(
      {
           host: 'localhost',
           user: 'root',
-          password: 'Z41icwvjk!',
+          password: '',
           database: 'election'
      },
      console.log('Connected to the election database')
