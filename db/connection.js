8eb512e082d4aaf5142165008969a1df1f72f328
Shawn Littrel <shawnlittrel@protonmail.com>
2021-04-13 20:08:28 -0700

    split server.js into 5 smaller apiRoutes files

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+//Connect to database
+const db = mysql.createConnection(
+     {
+          host: 'localhost',
+          user: 'root',
+          password: 'Z41icwvjk!',
+          database: 'election'
+     },
+     console.log('Connected to the election database')
+);
+
+
+module.exports = db;
